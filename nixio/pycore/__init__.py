c393268710ef7008af22924f6ec7f1f51ef77c65
Achilleas Koutsou <achilleas.k@gmail.com>
2016-04-18 15:24:58 +0300

    Init file for pycore
    Puts required classes at top of pycore namespace

---
+++ nixio/pycore/__init__.py
@@ -0,0 +1,13 @@
+from .file import File, FileMode
+from .block import Block
+from .property import DataType, Property
+from .section import Section
+from .value import Value
+from .source import Source
+from .data_array import DataArray
+from .dimensions import (RangeDimension, SetDimension, SampledDimnension,
+                         DimensionType)
+from .feature import Feature, LinkType
+from .tag import Tag
+from .multi_tag import MultiTag
+from .group import Group
