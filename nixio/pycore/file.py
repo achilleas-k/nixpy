7647144e6bd79dfd1e91354eb549acaad09348c4
Achilleas Koutsou <achilleas.k@gmail.com>
2016-04-18 21:05:05 +0300

    File object: Creation (open) and basic attributes

--- nixio/pycore/file.py
+++ nixio/pycore/file.py
@@ -5,6 +5,13 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
+from __future__ import absolute_import
+
+import h5py
+import numpy as np
+
+from . import util
+from . import Block
 
 
 class FileMode(object):
@@ -15,11 +22,41 @@ class FileMode(object):
 
 class File(object):
 
-    def __init__(self):
-        self.version = None
-        self.format = None
-        self.created_at = None
-        self.updated_at = None
+    _h5attrs = ["version", "format", "created_at", "updated_at"]
+
+    def __init__(self, path, mode=FileMode.ReadWrite):
+        self._h5file = h5py.File(name=path, mode=mode)
+        self.format = np.string_("nix")
+        self.version = [1, 0, 0]
+        self.created_at = util.nowstr()
+        self.updated_at = util.nowstr()
+        self._root = self._h5file["/"]
+        self._data = self._root["data"]
+
+    def __getattr__(self, item):
+        try:
+            if item not in self._h5attrs:
+                raise AttributeError
+            return self._h5obj.attrs.get(item)
+        except (KeyError, AttributeError):
+            raise AttributeError("{} has no attribute {}".format(
+                type(self), item
+            ))
+
+    def __setattr__(self, item, value):
+        try:
+            if item not in self._h5attrs:
+                raise AttributeError
+            # Check if item in _h5obj.attrs?
+            self._h5obj.attrs.modify(item, value)
+        except (KeyError, AttributeError):
+            raise AttributeError("{} has no attribute {}".format(
+                type(self), item
+            ))
+
+    @classmethod
+    def open(cls, path, mode=FileMode.ReadWrite):
+        return cls(path, mode)
 
     def force_created_at(self):
         pass
@@ -27,8 +64,14 @@ class File(object):
     def force_updated_at(self):
         pass
 
-    def create_block(self):
-        pass
+    def create_block(self, name, type_):
+        util.check_entity_name_and_type(name, type_)
+        if name in self:
+            raise ValueError("Block with the given name already exists!")
+        h5block = self._h5file.create_group(name)
+        block = Block(h5block, name, type_)
+        self._blocks[name] = block
+        return block
 
     def _block_count(self):
         pass
@@ -61,9 +104,6 @@ class File(object):
         pass
 
     def close(self):
-        pass
-
-    def open(self):
         pass
 
     def validate(self):
