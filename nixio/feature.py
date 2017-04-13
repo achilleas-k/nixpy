dbabd637f6a850fa967a796ff5e096fb38b0f162
Achilleas Koutsou <achilleas.k@gmail.com>
2017-04-13 16:57:18 +0200

    [DataArray,Tag,MultiTag] Move outside pycore/

--- nixio/feature.py
+++ nixio/feature.py
@@ -6,8 +6,8 @@
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
 from .entity import Entity
-from .data_array import DataArray
-from . import util
+from ..data_array import DataArray
+from .util import util
 
 
 class Feature(Entity):
--- nixio/tag.py
+++ nixio/tag.py
@@ -11,14 +11,14 @@ import numpy as np
 
 from .value import DataType
 from .data_array import DataArray
-from .entity_with_sources import EntityWithSources
-from .data_view import DataView
-from .feature import Feature
-from .exceptions import (OutOfBounds, IncompatibleDimensions,
-                         UninitializedEntity, InvalidUnit)
+from .pycore.entity_with_sources import EntityWithSources
+from .pycore.data_view import DataView
+from .pycore.feature import Feature
+from .pycore.exceptions import (OutOfBounds, IncompatibleDimensions,
+                                UninitializedEntity, InvalidUnit)
 from .dimension_type import DimensionType
 from .link_type import LinkType
-from . import util
+from .pycore import util
 
 from nixio.util.proxy_list import ProxyList, RefProxyList
 
