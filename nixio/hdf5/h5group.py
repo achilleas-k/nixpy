bfed954e938caf0cce2cff48dfd4366b27da8966
Achilleas Koutsou <achilleas.k@gmail.com>
2017-06-09 16:41:07 +0200

    [H5Group] Simpler error messages

--- nixio/hdf5/h5group.py
+++ nixio/hdf5/h5group.py
@@ -182,18 +182,14 @@ class H5Group(object):
         if self.group and name in self.group:
             return self.create_from_h5obj(self.group[name])
         else:
-            raise KeyError("No item with name {} found in {}".format(
-                name, self.group.name
-            ))
+            raise KeyError("Item not found '{}'".format(name))
 
     def get_by_id(self, id_):
         if self.group:
             for item in self:
                 if item.get_attr("entity_id") == id_:
                     return item
-        raise KeyError("No item with ID {} found in {}".format(
-            id_, self.name
-        ))
+        raise KeyError("Item not found '{}'".format(id_))
 
     def get_by_pos(self, pos):
         if not self.group:
